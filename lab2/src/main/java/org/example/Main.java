08f73a865d5921d87aeff58c02e00006235b25ff
Nibe34 <128199146+Nibe34@users.noreply.github.com>
2024-09-14 18:42:10 +0300

    lab2

---
+++ lab2/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import static org.example.Converter.convertRomanToArabic;
+
+/*
+    @author Nibe
+    @project Default (Template) Project
+    @class ${NAME}
+    @version ${VERSION}
+    @SE 14.09.2024 - 4:32
+*/
+public class Main {
+    public static void main(String[] args) {
+        convertRomanToArabic("XI");
+    }
+}
